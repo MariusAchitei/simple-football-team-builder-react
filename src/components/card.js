12d70fa61317083b77752a2a2490fdd53bfb8f48
Achitei Marius <marius.achitei02@gmail.com>
2023-03-14 22:39:16 +0200

    added screenshot

--- src/components/card.js
+++ src/components/card.js
@@ -30,29 +30,29 @@ export function Card({ player }) {
           <div className="stats">
             <ul>
               <li>
-                <span>89</span>
+                <span>{player.stats[0]}</span>
                 <span>pac</span>
               </li>
               <li>
-                <span>94</span>
+                <span>{player.stats[1]}</span>
                 <span>sho</span>
               </li>
               <li>
-                <span>81</span>
+                <span>{player.stats[2]}</span>
                 <span>pas</span>
               </li>
             </ul>
             <ul>
               <li>
-                <span>90</span>
+                <span>{player.stats[3]}</span>
                 <span>dri</span>
               </li>
               <li>
-                <span>33</span>
+                <span>{player.stats[4]}</span>
                 <span>def</span>
               </li>
               <li>
-                <span>83</span>
+                <span>{player.stats[5]}</span>
                 <span>phy</span>
               </li>
             </ul>
