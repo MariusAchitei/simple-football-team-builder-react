9d63efee8600c4d94cc1f6c3d190688d53bdfc97
Achitei Marius <marius.achitei02@gmail.com>
2023-03-14 22:30:09 +0200

    changed readme

--- src/components/field.js
+++ src/components/field.js
@@ -7,113 +7,113 @@ export function Field({ formation }) {
   let players = [
     {
       name: "Tarnovanu",
-      value: "94",
-      position: "ST",
+      value: "67",
+      position: "GK",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/568544-1631086819.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [65, 67, 65, 66, 41, 67],
     },
     {
       name: "Sorescu",
-      value: "94",
-      position: "ST",
+      value: "69",
+      position: "RB",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/379209-1627301347.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [86, 66, 64, 68, 61, 73],
     },
     {
       name: "Tamm",
-      value: "94",
-      position: "ST",
+      value: "66",
+      position: "CB",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/111556-1641743392.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [60, 52, 45, 51, 64, 79],
     },
     {
       name: "Cristea",
-      value: "94",
-      position: "ST",
+      value: "70",
+      position: "CB",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/331971-1631086911.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [74, 38, 46, 55, 70, 77],
     },
     {
       name: "Radunovic",
-      value: "94",
-      position: "ST",
+      value: "69",
+      position: "LB",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/144339-1631086944.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [77, 57, 63, 60, 65, 78],
     },
     {
       name: "EDJOUMA",
-      value: "94",
-      position: "ST",
+      value: "68",
+      position: "CM",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/468704-1652260774.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [63, 55, 57, 65, 66, 78],
     },
     {
       name: "Sut",
-      value: "94",
+      value: "67",
       position: "ST",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/433979-1631087307.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [71, 61, 61, 63, 65, 71],
     },
     {
       name: "Olaru",
-      value: "94",
-      position: "ST",
+      value: "72",
+      position: "CM",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/395800-1631087222.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [82, 70, 72, 73, 63, 79],
     },
     {
       name: "Cordea",
-      value: "94",
-      position: "ST",
+      value: "68",
+      position: "RW",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/435712-1631088546.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [85, 64, 65, 70, 36, 67],
     },
     {
       name: "Compagno",
-      value: "94",
+      value: "66",
       position: "ST",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/287255-1661929174.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [47, 66, 46, 58, 32, 76],
     },
     {
       name: "Popescu",
-      value: "94",
-      position: "ST",
+      value: "72",
+      position: "LW",
       photo:
         "https://img.a.transfermarkt.technology/portrait/header/797100-1631088497.png?lm=1",
       club: "/clubs/steaua.png",
       country: "/flags/romania.png",
-      stats: [99, 99, 99, 99, 99, 99],
+      stats: [82, 68, 69, 76, 31, 53],
     },
   ];
 
@@ -133,11 +133,6 @@ export function Field({ formation }) {
   const dragEnd = (e) => {
     e.stopPropagation();
     console.log("DragEnd");
-
-    // targetCard = currentCard;
-
-    // let target = cloneElement(targetCard.firstElementChild);
-    // let current = cloneElement(currentCard.firstElementChild);
 
     let target = targetCard.cloneNode(true);
     let current = currentCard.cloneNode(true);
